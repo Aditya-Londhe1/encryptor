2e1fb51c007bfbd39f8dae040ac3bc4d52fcbe7c
Aditya Londhe <aditya.londhe24@sanjivani.edu.in>
2025-03-30 10:51:34 +0530

    app.py

--- app.py
+++ app.py
@@ -4,9 +4,10 @@ import string
 import hashlib
 import os
 
+app = Flask(__name__)
 port = int(os.environ.get("PORT", 10000))  # Default to 10000 if PORT is not set
 app.run(host="0.0.0.0", port=port, debug=True)
-app = Flask(__name__)
+
 
 def generate_mapping(key: str):
     """Generate encryption & decryption mappings based on a given key."""
